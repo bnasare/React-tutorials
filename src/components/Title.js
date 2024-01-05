0192b7f429ce7321b94d758fa74a049b23919ea9
bnasare <124492150+bnasare@users.noreply.github.com>
2024-01-05 23:24:07 +0000

    clean up

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,5 @@
 import React from 'react'
 
-// const Title = (props) => {
-//     return (
-//         <div className="section-title">
-//             <h2>{props.firstTitle}<span> {props.secondTitle}</span></h2>
-//         </div>
-//     )
-// }
-
 const Title = ({ firstTitle, secondTitle }) => {
     return (
         <div className="section-title">
